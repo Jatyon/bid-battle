1e4bf53070478a837ca27e59a890ab14e0db94e9
Jatyon <bialy199912@gmail.com>
2026-03-01 11:49:46 +0100

    refactor(auth): refactor dto messages

--- server/src/modules/auth/dto/auth-refresh-token.dto.ts
+++ server/src/modules/auth/dto/auth-refresh-token.dto.ts
@@ -1,7 +1,7 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class RefreshTokenDto {
-  @IsString({ message: 'validation.token_is_string' })
-  @IsNotEmpty({ message: 'validation.token_not_empty' })
+  @IsString({ message: 'error.validation.token_is_string' })
+  @IsNotEmpty({ message: 'error.validation.token_not_empty' })
   refreshToken: string;
 }
--- server/src/modules/auth/dto/auth-reset-password.dto.ts
+++ server/src/modules/auth/dto/auth-reset-password.dto.ts
@@ -2,16 +2,16 @@ import { Match } from '@core/decorators/validator/match.decorator';
 import { IsString, Matches, MinLength } from 'class-validator';
 
 export class AuthResetPasswordDto {
-  @IsString({ message: 'validation.token_is_string' })
+  @IsString({ message: 'error.validation.token_is_string' })
   token: string;
 
-  @IsString({ message: 'validation.password_not_empty' })
-  @MinLength(8, { message: 'validation.password_at_least_8_characters' })
+  @IsString({ message: 'error.validation.password_not_empty' })
+  @MinLength(8, { message: 'error.validation.password_at_least_8_characters' })
   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
-    message: 'validation.password_too_weak',
+    message: 'error.validation.password_too_weak',
   })
   password: string;
 
-  @Match('password', { message: 'validation.passwords_do_not_match' })
+  @Match('password', { message: 'error.validation.passwords_do_not_match' })
   passwordRepeat: string;
 }
