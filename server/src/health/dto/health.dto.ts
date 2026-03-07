2d445d317953b75aaeda174557c6210346eaf52a
Jatyon <bialy199912@gmail.com>
2026-03-07 12:22:23 +0100

    feat(health): add health to swagger

--- server/src/health/dto/health.dto.ts
+++ server/src/health/dto/health.dto.ts
@@ -1,6 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class HealthDto {
-  @ApiProperty({ example: 'UP', description: 'Serwera status' })
+  @ApiProperty({
+    example: 'UP',
+    description: 'Server status',
+  })
   status: string;
 }
--- server/src/health/health.controller.ts
+++ server/src/health/health.controller.ts
@@ -1,10 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { ApiStandardResponse } from '@core/decorators/api-standard-response.decorator';
 import { Public } from '@core/decorators/public.decorator';
 import { HealthDto } from './dto';
 
+@ApiTags('Health')
 @Controller('health')
 export class HealthController {
+  @ApiOperation({
+    summary: 'Health check',
+    description: 'Check if the server is running and healthy',
+  })
   @Get()
   @Public()
   @ApiStandardResponse(HealthDto, false)
