fd8fee6438fff9e78b5758fae117abab7910e4a0
Jatyon <bialy199912@gmail.com>
2026-03-31 18:21:25 +0200

    refactor(health): remove throttle for health.controller

--- server/src/health/health.controller.ts
+++ server/src/health/health.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { SkipThrottle } from '@nestjs/throttler';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { ApiStandardResponse, Public } from '@core/decorators';
 import { HealthDto } from './dto';
 
 @ApiTags('Health')
+@SkipThrottle()
 @Controller('health')
 export class HealthController {
   @ApiOperation({
