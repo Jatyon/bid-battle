bf0b3080c5617a9305b2912aab06fba125edaa96
Jatyon <bialy199912@gmail.com>
2026-04-24 17:46:01 +0200

    chore(translation): add transloco lib and config translations

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,6 +1,9 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { Language, TranslocoHttpLoader } from '@core/index';
+import { provideTransloco } from '@ngneat/transloco';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -8,5 +11,15 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: [Language.EN, Language.PL],
+        defaultLang: Language.EN,
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
   ],
 };
