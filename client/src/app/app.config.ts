c1d3d327213f015e43cd385c6e520353218691ba
Jatyon <bialy199912@gmail.com>
2026-04-25 10:44:25 +0200

    feat(router): enable component input binding and view transitions

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,7 +1,7 @@
+import { provideRouter, TitleStrategy, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
-import { provideRouter, TitleStrategy } from '@angular/router';
 import { Language, TranslocoHttpLoader, AppTitleStrategy } from '@core/index';
 import { provideTransloco } from '@ngneat/transloco';
 import { routes } from './app.routes';
@@ -9,7 +9,11 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+     provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withViewTransitions(),
+    ),
     provideClientHydration(withEventReplay()),
     provideHttpClient(),
     provideTransloco({
