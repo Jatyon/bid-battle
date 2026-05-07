07b31c1c44f6b6c79db16fc4205e30aac0628bd4
Jatyon <bialy199912@gmail.com>
2026-05-07 18:18:52 +0200

    feat(http): support SKIP_LOADING token in loading interceptor

--- client/src/app/core/interceptors/loading.interceptor.ts
+++ client/src/app/core/interceptors/loading.interceptor.ts
@@ -1,12 +1,16 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { LoadingService } from '@core/services';
+import { SKIP_LOADING } from './http-context.tokens';
 import { finalize } from 'rxjs';
 
 /**
  * Increments / decrements the global loading counter around every HTTP request.
+ * Requests with `SKIP_LOADING` context token set to `true` are excluded.
  */
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
+  if (req.context.get(SKIP_LOADING)) return next(req);
+
   const loadingService = inject(LoadingService);
 
   loadingService.start();
